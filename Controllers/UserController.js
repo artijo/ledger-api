eb96cf780e685fba186766e15c8a5f700fe35931
tijo <81065469+artijo@users.noreply.github.com>
2024-05-01 14:06:46 +0700

    Update UserController.js

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -18,8 +18,37 @@ const getbyemail = async (req, res) => {
         res.json({message: error.message}).status(500);
     }
 }
+const create = async (req, res) => {
+    try{
+        let user = new User(req.body);
+        await user.save();
+        res.json(user);
+    } catch (error) {
+        res.json({message: error.message}).status(500);
+    }
+}
+const update = async (req, res) => {
+    try{
+        let user = await User.findOneAndUpdate({email: req.params.email}, req.body, {new: true});
+        res.json(user);
+    }catch (error) {
+        res.json({message: error.message}).status(500);
+    }
+}
+const remove = async (req, res) => {
+    try{
+        let user = await User.findOneAndDelete({email: req.params.email});
+        res.json(user);
+    }
+    catch (error) {
+        res.json({message: error.message}).status(500);
+    }
+}
 
 module.exports = {
     getall,
-    getbyemail
+    getbyemail,
+    create,
+    update,
+    remove
 }
