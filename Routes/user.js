930620bdb4fe688ea063961c60949886468cd3e3
tijo <81065469+artijo@users.noreply.github.com>
2024-05-01 14:08:36 +0700

    Update user.js

--- Routes/user.js
+++ Routes/user.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const userController = require('../Controllers/UserController');
 
 router.get('/users', userController.getall);
+router.get('/users/:email', userController.getbyemail);
+router.post('/users', userController.create);
+router.put('/users/:email', userController.update);
+router.delete('/users/:email', userController.remove);
 
 module.exports = {
     router
